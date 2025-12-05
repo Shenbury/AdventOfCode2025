3a12a8cc6c153a42b2b34adec096a12b35ba3486
Simon Henbury <HenburS@rac.co.uk>
2025-12-05 21:31:53 +0000

    update

---
+++ AdventOfCode2025/Day1-1/Program.cs
@@ -0,0 +1,45 @@
+﻿int[] safeDial = [.. Enumerable.Range(0, 100)];
+int currentPosition = 50;
+
+string[] instructions = ParseInput();
+
+int zeroCount = 0;
+
+foreach (string toDo in instructions)
+{
+    char direction = toDo[0];
+    int steps = int.Parse(toDo[1..]);
+    if (direction == 'L')
+    {
+        TurnLeft(steps);
+    }
+    else if (direction == 'R')
+    {
+        TurnRight(steps);
+    }
+
+    if (currentPosition == 0)
+    {
+        zeroCount++;
+    }
+}
+
+Console.WriteLine($"Current Position: {currentPosition}, Zero Count: {zeroCount}");
+
+int TurnLeft(int steps)
+{
+    currentPosition = (currentPosition - steps + safeDial.Length) % safeDial.Length;
+    return currentPosition;
+}
+
+int TurnRight(int steps)
+{
+    currentPosition = (currentPosition + steps) % safeDial.Length;
+    return currentPosition;
+}
+
+string[] ParseInput()
+{
+    string input = File.ReadAllText("input.txt");
+    return input.Split(Environment.NewLine);
+}
---
+++ AdventOfCode2025/Day1-2/Program.cs
@@ -0,0 +1,56 @@
+﻿int[] safeDial = [.. Enumerable.Range(0, 100)];
+int currentPosition = 50;
+
+string[] instructions = ParseInput();
+
+int zeroCount = 0;
+
+foreach (string toDo in instructions)
+{
+    char direction = toDo[0];
+    int steps = int.Parse(toDo[1..]);
+    if (direction == 'L')
+    {
+        TurnLeft(steps);
+    }
+    else if (direction == 'R')
+    {
+        TurnRight(steps);
+    }
+}
+
+Console.WriteLine($"Current Position: {currentPosition}, Zero Count: {zeroCount}");
+
+int TurnLeft(int steps)
+{
+    for (int i = 0; i < steps; i++)
+    {
+        currentPosition = (currentPosition - 1 + safeDial.Length) % safeDial.Length;
+        if (currentPosition == 0)
+        {
+            zeroCount++;
+        }
+    }
+
+    return currentPosition;
+}
+
+int TurnRight(int steps)
+{
+    for (int i = 0; i < steps; i++)
+    {
+        currentPosition = (currentPosition + 1) % safeDial.Length;
+        if (currentPosition == 0)
+        {
+            zeroCount++;
+        }
+    }
+
+    return currentPosition;
+}
+
+string[] ParseInput()
+{
+    string input = File.ReadAllText("input.txt");
+    return input.Split(Environment.NewLine);
+}
