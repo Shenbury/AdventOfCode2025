b186610a0be8cff2cda8d68c487c86f0718e04ff
Shenbury <simonhenbury@live.co.uk>
2025-12-06 21:08:59 +0000

    day 2

---
+++ AdventOfCode2025/Day2-1/Program.cs
@@ -0,0 +1,68 @@
+﻿string[] instructions = ParseInput();
+
+long sumOfInvalidIds = 0;
+
+foreach (var instruction in instructions)
+{
+    string[] parts = instruction.Split("-");
+    long start = long.Parse(parts[0]);
+    long end = long.Parse(parts[1]);
+
+    long[] rangeNumbersToCheck = [.. Enumerable.Range(start, end)];
+
+    foreach (var number in rangeNumbersToCheck)
+    {
+        long invalidOr0ToSum = FindRecurringCharacterArrayInANumber(number);
+        sumOfInvalidIds += invalidOr0ToSum;
+    }
+}
+
+Console.WriteLine($"Sum of Invalid Ids: {sumOfInvalidIds}");
+Console.WriteLine($"Sum of Invalid Ids is Correct To Example: {sumOfInvalidIds == 4174379265}");
+
+
+long FindRecurringCharacterArrayInANumber(long number) {
+    long numberLength = number.ToString().ToCharArray().Length;
+
+    if (numberLength <= 1)
+    {
+        return 0;
+    }
+
+    long numberLengthHalved = numberLength / 2;
+    long firstHalf = long.Parse(number.ToString()[..(int)numberLengthHalved]);
+    long secondHalf = long.Parse(number.ToString()[(int)numberLengthHalved..]);
+
+    if((firstHalf.ToString().Length + secondHalf.ToString().Length) != numberLength)
+    {
+        return 0;
+    }
+
+    if (firstHalf == secondHalf)
+    {
+        return number;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+string[] ParseInput()
+{
+    string input = File.ReadAllText("input.txt");
+    return input.Split(",");
+}
+
+public static class Enumerable 
+{
+    public static long[] Range(long startNumber, long endNumber)
+    {
+        long[] result = new long[endNumber - startNumber + 1];
+        for (long i = startNumber; i <= endNumber; i++)
+        {
+            result[i - startNumber] = i;
+        }
+        return result;
+    }
+}
---
+++ AdventOfCode2025/Day2-2/Program.cs
@@ -0,0 +1,49 @@
+﻿long result = 0;
+List<List<long>> ListOfRanges = [];
+
+List<string> Inputlist = [.. ParseInput()];
+foreach (string s in Inputlist)
+{
+    List<string> StartAndEndOfRange = [.. s.Split('-')];
+    long start = long.Parse(StartAndEndOfRange[0]);
+    long end = long.Parse(StartAndEndOfRange[1]);
+    List<long> Range = [];
+
+    for (long i = start; i < end + 1; i++)
+    {
+        Range.Add(i);
+    }
+    ListOfRanges.Add(Range);
+};
+
+foreach (List<long> Range in ListOfRanges)
+{
+    foreach (long Id in Range)
+    {
+        int Increment = 1;
+        string IdString = Id.ToString();
+        while (Increment <= IdString.Length / 2)
+        {
+            if (Id.ToString().Length % Increment == 0)
+            {
+                string Block = Id.ToString()[..Increment];
+                string RepeatedBlock = string.Concat(Enumerable.Repeat(Block, IdString.Length / Increment));
+                if (RepeatedBlock == IdString)
+                {
+                    result += Id;
+                    break;
+                }
+            }
+            Increment++;
+        }
+    }
+};
+
+Console.WriteLine($"Sum of Invalid Ids: {result}");
+
+
+static string[] ParseInput()
+{
+    string input = File.ReadAllText("input.txt");
+    return input.Split(",");
+}
