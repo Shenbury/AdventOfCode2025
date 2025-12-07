4ab593ed0964e079d0100455ef70ecd10a8f10fc
Shenbury <simonhenbury@live.co.uk>
2025-12-07 13:36:14 +0000

    day 3

---
+++ AdventOfCode2025/Day3-1/Program.cs
@@ -0,0 +1,49 @@
+﻿var banks = ParseInput();
+
+int sumOfVoltages = 0;
+
+foreach (string bank in banks)
+{
+    int[] batteriesInBank = [.. bank.ToCharArray().Select(x => int.Parse(x.ToString()))];
+
+    if (batteriesInBank.Length > 1)
+    {
+        int batteryCount = batteriesInBank.Length;
+        int maximum = batteriesInBank.Max(x => x);
+        int maxValueIndex = Array.FindIndex(batteriesInBank, 0, batteryCount, row => row == maximum) + 1;
+
+        string subString = string.Join("", batteriesInBank)[maxValueIndex..];
+        string subStringWithLastEntry = string.Join("", batteriesInBank).Substring(0, maxValueIndex - 1);
+
+        string resultString = string.Empty;
+
+        if (batteriesInBank.Length == 1)
+        {
+            resultString = maximum.ToString();
+        }
+        else if (maxValueIndex == batteriesInBank.Length)
+        {
+            var secondMax = subStringWithLastEntry.ToCharArray().Select(x => int.Parse(x.ToString())).Max(x => x);
+
+            resultString = secondMax.ToString() + maximum.ToString();
+        }
+        else if (batteriesInBank.Length > 1)
+        {
+            var secondMax = subString.ToCharArray().Select(x => int.Parse(x.ToString())).Max(x => x);
+
+            resultString = maximum.ToString() + secondMax.ToString();
+        }
+
+        int biggestVoltage = int.Parse(resultString);
+
+        sumOfVoltages += biggestVoltage;
+    }
+}
+
+Console.WriteLine(sumOfVoltages);
+
+static string[] ParseInput()
+{
+    string input = File.ReadAllText("input.txt");
+    return input.Split(Environment.NewLine);
+}
---
+++ AdventOfCode2025/Day3-2/Program.cs
@@ -0,0 +1,49 @@
+﻿var banks = ParseInput();
+
+long sumOfVoltages = 0;
+
+foreach (string bank in banks)
+{
+    long[] batteriesInBank = [.. bank.ToCharArray().Select(x => int.Parse(x.ToString()))];
+
+    if (batteriesInBank.Length > 0)
+    {
+        int numbersToTake = Math.Min(12, batteriesInBank.Length);
+        List<long> selectedNumbers = [];
+        long currentIndex = 0;
+
+        for (int i = 0; i < numbersToTake; i++)
+        {
+            long remainingSlots = numbersToTake - i;
+            long maxSearchIndex = batteriesInBank.Length - remainingSlots;
+
+            long maxValue = long.MinValue;
+            long maxIndex = currentIndex;
+            
+            for (long j = currentIndex; j <= maxSearchIndex; j++)
+            {
+                if (batteriesInBank[j] > maxValue)
+                {
+                    maxValue = batteriesInBank[j];
+                    maxIndex = j;
+                }
+            }
+            
+            selectedNumbers.Add(maxValue);
+            currentIndex = maxIndex + 1;
+        }
+
+        string resultString = string.Join("", selectedNumbers);
+        long biggestVoltage = long.Parse(resultString);
+
+        sumOfVoltages += biggestVoltage;
+    }
+}
+
+Console.WriteLine(sumOfVoltages);
+
+static string[] ParseInput()
+{
+    string input = File.ReadAllText("input.txt");
+    return input.Split(Environment.NewLine);
+}
